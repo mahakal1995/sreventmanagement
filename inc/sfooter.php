499dca72af21e71a5cafcce605157a704694e5a9
mahakal1995 <37874176+mahakal1995@users.noreply.github.com>
2022-04-29 11:48:07 +0530

    web

---
+++ inc/sfooter.php
@@ -0,0 +1,3 @@
+<div class="stu_footer">
+    <div class="stu_footer_p">&copy; <a href="/">arctik circle</a> | Developed by falgun and krutarth</div>
+</div>
---
+++ singup_instructor.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teach Online | MK ELearn</title>
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+
+<!-- Header Section Start -->
+    <?php include_once('inc/header.php') ?>
+<!-- Header Section End -->
+
+<!-- Section 1 Start  -->
+<div class="int_sec1">
+    <div class="int_sec1_bnr">
+        <img src="images/site/instructor_bnr.jpg" alt="instructor_banner">
+    </div>
+    <div class="int_sec1_bnr_txt">
+        <div class="int_sec1_bnr_midd">
+            <div class="int_sec1_ttl">Make a global impact</div>
+            <div class="int_sec1_p"></div>
+            <a href="instructor/signup" class="int_sec1_btn">Become an Instructor</a>
+        </div>
+    </div>
+</div>
+<!-- Section 1 End -->
+
+
+<!-- Section 2 Start  -->
+<div class="int_sec2">
+    <div class="int_sec2_ttl">Discover your potential</div>
+    <div class="int_sec2_abt">
+        <div class="int_sec_abt1">
+            <div class="int_sec_abt_sub"><i class="fas fa-rupee-sign fa-4x int_sec2_icon"></i></div>
+            <div class="int_sec_abt_sub1">Earn money</div>
+            <div class="int_sec_abt_sub2"></div>
+        </div>
+        <div class="int_sec_abt1">
+            <div class="int_sec_abt_sub"><i class="far fa-play-circle fa-4x int_sec2_icon"></i></div>
+            <div class="int_sec_abt_sub1">Inspire students</div>
+            <div class="int_sec_abt_sub2"></div>
+        </div>
+        <div class="int_sec_abt1">
+            <div class="int_sec_abt_sub"><i class="far fa-thumbs-up fa-4x int_sec2_icon"></i></div>
+            <div class="int_sec_abt_sub1">Join our community</div>
+            <div class="int_sec_abt_sub2"></div>
+        </div>
+    </div>
+</div>
+<!-- Section 2 End -->
+
+
+<!-- Section 3 Start -->
+
+    <div class="int_sec3">
+        <div class="int_sec3_img">
+            <img src="images/site/world_map.jpg" alt="world_map">
+        </div>
+        <div class="int_sec3_txt">
+            <div class="int_sec3_txt2">
+                <div class="int_sec3_ttl">Exceptional opportunities</div>
+               
+            </div>
+        </div>
+    </div>
+
+<!-- Section 3 End -->
+
+
+<!-- Section 4 Start -->
+
+    <div class="int_sec4">
+        <div class="int_sec4_mid">
+            <div class="int_sec4_ttl">Envision your success</div>
+                <!-- Details Summary Int Sec4 Start  -->
+                <div class="int_sec4_details">
+                    <details class="int_sec4_details1">
+                        <summary class="int_sec4_det_summ"><span>&#9312;</span> Plan your course</summary>
+                            <div class="int_sec4_tb_ttl int_sum_ttl">Plan your course</div>
+                            <div class="int_sec4_tb_tag">Get organized</div>
+                            <div class="int_sec4_tb_txt">
+                               
+                            </div>
+                    </details>
+                    <details class="int_sec4_details1">
+                        <summary class="int_sec4_det_summ"><span>&#9313;</span> Record your video</summary>
+                            <div class="int_sec4_tb_ttl int_sum_ttl">Record your video</div>
+                            <div class="int_sec4_tb_tag">Get organized</div>
+                            <div class="int_sec4_tb_txt">
+                                
+                            </div>
+                    </details>
+                    <details class="int_sec4_details1">
+                        <summary class="int_sec4_det_summ"><span>&#9314;</span> Build your community</summary>
+                            <div class="int_sec4_tb_ttl int_sum_ttl">Build your community</div>
+                            <div class="int_sec4_tb_tag">Get organized</div>
+                            <div class="int_sec4_tb_txt">
+                               
+                            </div>
+                    </details>
+                </div>
+                <!-- Details Summary Int Sec4 End  -->
+                
+
+                <div class="int_sec4_bdy">
+                    <div class="int_sec4_fst">
+                        <div class="int_sec4_ic">
+                            <span class="int_sec4_icon"><i class="fas fa-mouse-pointer fa-2x"></i></span>
+                            <div class="int_Sec4_ic_txt"><span>&#9312;</span> Plan your course</div>
+                        </div>
+                        <div class="int_sec4_ic">
+                            <span class="int_sec4_icon"><i class="fas fa-play-circle fa-2x"></i></span>
+                            <div class="int_Sec4_ic_txt"><span>&#9313;</span> Record your video</div>
+                        </div>
+                        <div class="int_sec4_ic">
+                            <span class="int_sec4_icon"><i class="fas fa-comments fa-2x"></i></span>
+                            <div class="int_Sec4_ic_txt"><span>&#9314;</span> Build your community</div>
+                        </div>
+                    </div>
+
+                    <div class="int_sec4_tb sec4_tb1">
+                        <div class="int_sec4_tb_pt1">
+                            <div class="int_sec4_tb_ttl">Plan your course</div>
+                            <div class="int_sec4_tb_tag">Get organized</div>
+                            <div class="int_sec4_tb_txt">
+                               
+                            </div>
+                        </div>
+                        <div class="int_Sec4_tb_img">
+                            <img src="images/" alt="">
+                        </div>
+                    </div>
+
+                    <div class="int_sec4_tb sec4_tb2">
+                        <div class="int_sec4_tb_pt1">
+                            <div class="int_sec4_tb_ttl">Record your video</div>
+                            <div class="int_sec4_tb_tag">Get o Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta provident, est, qui fuga minus corrupti, excepturi magnam iusto incidunt amet error dolor non quibusdam quos! Incidunt, maiores sed? Dolorem quis culpa ducimus officiis beatae facere temporibus quam quas voluptate esse, earum soluta aspernatur repellat sapiente laboriosam possimus nisi minima suscipit, minus tenetur pariatur. Quia suscipit numquam distinctio recusandae, quaerat sapiente perspiciatis blanditiis quo dolore itaque? Incidunt ratione enim est. Labore vitae a voluptatem omnis eveniet excepturi enim magni corporis voluptates natus quia id quasi sunt eaque, dolor repellat ad consequatur iste inventore minima deserunt! Id sit necessitatibus voluptas aspernatur voluptates!
+                            </div>
+                        </div>rganized</div>
+                            <div class="int_sec4_tb_txt">
+                               
+                        <div class="int_Sec4_tb_img">
+                            <img src="images/" alt="">
+                        </div>
+                    </div>
+
+                    <div class="int_sec4_tb sec4_tb3">
+                            <div class="int_sec4_tb_pt1">
+                            <div class="int_sec4_tb_ttl">Build your community</div>
+                            <div class="int_sec4_tb_tag">Get organized</div>
+                            <div class="int_sec4_tb_txt">
+                                
+                            </div>
+                        <div class="int_Sec4_tb_img">
+                            <img src="images/" alt="">
+                        </div>
+                    </div>
+                </div>  
+        </div>
+    </div>
+
+<!-- Section 4 End -->
+
+
+<!-- Section 5 Start -->
+<div class="int_sec5">
+    <div class="int_sec5_img">
+        <img src="images/site/working.jpg" alt="service">
+    </div>
+    <div class="int_sec5_txt">
+       <div class="int_sec5_txt1">
+            <div class="int_sec5_ttl">We're here to help</div>
+            <div class="int_sec5_bdy">arctik circle will always be helpful to you and will increase your knowledge.....</div>
+       </div>
+    </div>
+</div>
+<!-- Section 5 End -->
+
+
+<!-- Section 6 Start -->
+
+<div class="int_sec6">
+    <div class="int_sec6_bdy">
+        <div class="int_sec6_ttl">Become an instructor today</div>
+        <div class="int_sec6_tag">Join the world's largest online learning marketplace.</div>
+        <a href="instructor/signup" class="int_sec6_btn">Get Started</a>
+    </div>
+</div>
+
+<!-- Section 6 End -->
+
+
+<div class="exp">
+</div>
+
+
+<!-- Footer Section Start -->
+<?php include_once('inc/footer.php') ?>
+<!-- Footer Section End -->
+    <script src="javascript/jquery.js"></script>
+    <script src="javascript/all.js"></script>
+    <script src="javascript/frontend.js"></script>
+</body>
+</html>
